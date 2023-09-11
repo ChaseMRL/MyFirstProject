b7ff84ddb45032d90aebf00b3b0202e61d6e3b1f
ChaseMRL <deiter1@tritons.iowacentral.edu>
2023-09-11 10:45:00 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+	Chase Deiter
+	C++ Semester Year
+	Due: 9/15/2023
+	Lab 1
+
+	This lab will serve as a demonstration of your ability to download a project from
+GitHub, compile and run the project, upload a project to GitHub, and submit a project to Canvas.
+This lab assumes you have downloaded and installed an IDE (integrated development
+environment) and have also installed GitHub Desktop. If you have not met the assumptions for
+the lab, the steps you need to complete the lab may vary from what is here. The goals are for
+you to download a project, compile and run that project, and to upload your own project. If you
+meet the goals, you will get the points for this lab. Follow the directions in this lab and complete
+the tasks. If you have questions, please ask.
+*/
